0b5c3c0c95dc5d9243793bb4a7e635c86090e91c
Maggie Storey <maggiestorey@Maggies-Air.lan>
2024-02-07 11:57:45 -0800

    second commit

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,43 +1,28 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Button from './Button';
-import { signInWithPopup, signOut, onAuthStateChanged, User } from 'firebase/auth'; 
-import { auth, provider } from '../config/firebase';
-
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Button from './Button'
+import { signInWithPopup, signOut } from 'firebase/auth'
+import { auth, provider } from '../config/firebase'
 function Navbar() {
-    console.log("Navbar component is rendering");
-
-    const [isVisible, setIsVisible] = useState(false);
-    const [currentUser, setCurrentUser] = useState<User | null>(null); 
-
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            setCurrentUser(user);
-        });
-
-        return () => unsubscribe();
-    }, []);
-
-    const signOutOnClick = async () => {
-        try {
-            await signOut(auth);
-            setCurrentUser(null);
-        } catch (error) {
-            console.error('Error signing out:', error);
-        }
+  const [isVisible, setIsVisible] = useState(false)
+  const signOutOnClick = () => {
+    signOut(auth)
+    location.reload();
+  }
+  const signInOnClick = async () => {
+    // const response = await signInWithPopup(auth, Providers.google);
+    const response = await signInWithPopup(auth, provider);
+    if ( response.user ) {
+        location.reload();
     }
-    
+  }
+  const dropDown = () => {
+    setIsVisible(!isVisible)
+  }
+  const clicked = () => {
+    setIsVisible(false)
+  }
 
-    const signInOnClick = async () => {
-        const response = await signInWithPopup(auth, provider);
-        if (response.user) {
-            setCurrentUser(response.user);
-        }
-    }
-
-    const dropDown = () => {
-        setIsVisible(!isVisible);
-    }
 
     return (
         <nav className='flex items-center justify-between flex-wrap bg-teal-500 p-6'>
@@ -46,21 +31,35 @@ function Navbar() {
             </div>
             <div className='block'>
                 <button onClick={dropDown} className='flex items-center px-3 py-2 text-teal-200 border rounded border-teal-400 hover:text-white hover:border-white'>
-                    <i className='fas fa-bars'>menu</i>
+                    
+                    <i className='fas fa-bars'>Options</i>
                 </button>
             </div>
-            {isVisible ? (
-                <div className='w-full block flex-grow items-center'>
-                    <div className="text-sm lg:flex-grow">
-                        {currentUser ? (
-                            <Button className='p-3 m-5 bg-teal-400 justify-center'>
-                                <div>
-                                    <Link to="/" onClick={signOutOnClick} className='flex place-items-center mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white'>
-                                        Sign Out
-                                    </Link>
-                                </div>
-                            </Button>
-                        ) : (
+            { isVisible ? (
+            <div className='w-full block flex-grow items-center'>
+                <div className="text-sm lg:flex-grow">
+                    <Button className='p-3 m-5 bg-teal-400 justify-center'>
+                        <div>
+                            <Link to='/' onClick={ clicked} className='flex place-itmes-center mt-4 lg:inline-block lg:mt-0
+                             text-teal-200 hover:text-white mr-4'>
+                                
+                            </Link>
+                        </div>
+
+                    </Button>
+                    <Button className='p-3 m-5 bg-teal-400 justify-center'>
+                        <div>
+                            <Link to='/about' onClick={ clicked} className='flex place-itmes-center mt-4 lg:inline-block lg:mt-0
+                             text-teal-200 hover:text-white mr-4'>
+                                
+                            </Link>
+                        </div>
+
+                    </Button>
+                    
+
+                        {
+                            !auth.currentUser ? 
                             <Button className='p-3 m-5 bg-teal-400 justify-center'>
                                 <div>
                                     <Link to="/" onClick={signInOnClick} className='flex place-items-center mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white'>
@@ -68,7 +67,15 @@ function Navbar() {
                                     </Link>
                                 </div>
                             </Button>
-                        )}
+                            :
+                            <Button className='p-3 m-5 bg-teal-400 justify-center'>
+                                <div>
+                                    <Link to="/" onClick={signOutOnClick} className='flex place-items-center mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white'>
+                                        Sign Out
+                                    </Link>
+                                </div>
+                            </Button>
+                        }
                     </div>
                 </div>
             ) : (
